8ab2442b99be8bcf36afdbbe213a26699a828988
Joshua Foss <jmfoss98@gmail.com>
2019-11-05 19:53:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,10 @@
     $getResults= sqlsrv_query($conn, $tsql);
     echo ("Reading data from table" . PHP_EOL);
     if ($getResults == FALSE)
-        echo "not working";
         echo (sqlsrv_errors());
     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) 
     {
-       echo "it is working";
+       echo &row;
        echo ($row['userID'] . " " . $row['username'] . " " . $row['password'] . " " . $row['join_date'] . PHP_EOL);
     }
     sqlsrv_free_stmt($getResults);
