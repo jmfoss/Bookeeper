2c7064259cad3e2056529b3f2bee5575fa7f3894
Joshua Foss <jmfoss98@gmail.com>
2019-11-05 19:27:22 -0500

    add sql connection

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
 echo "We can do this!";
+
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:bookeeper.database.windows.net,1433; Database = Bookeeper", "jmfoss", "Mikito98");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "jmfoss", "pwd" => "{your_password_here}", "Database" => "Bookeeper", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:bookeeper.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
