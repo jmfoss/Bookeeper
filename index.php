10931954637731957518b047f8d41d28dab5dffc
Joshua Foss <jmfoss98@gmail.com>
2019-11-05 20:41:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     $conn = sqlsrv_connect($serverName, $connectionOptions);
     $tsql= "SELECT * FROM users";
     $getResults= sqlsrv_query($conn, $tsql);
-    echo ("Reading data from table" . PHP_EOL);
+    echo ("Reading data from table\n" . PHP_EOL);
     if ($getResults == FALSE)
         echo (sqlsrv_errors());
     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) 
