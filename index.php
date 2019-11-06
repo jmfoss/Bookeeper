267949e4a6b5a1a20654867d312e2135ee8bb052
Joshua Foss <jmfoss98@gmail.com>
2019-11-05 20:02:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,8 @@
         echo (sqlsrv_errors());
     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) 
     {
-       echo $row;
+       echo $row['userID'];
+       
        echo ($row['userID'] . " " . $row['username'] . " " . $row['password'] . " " . $row['join_date'] . PHP_EOL);
     }
     sqlsrv_free_stmt($getResults);
