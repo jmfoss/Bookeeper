944fa66676f8dc84cc0e8a6d282bfe00ceb2521a
Joshua Foss <jmfoss98@gmail.com>
2019-11-05 20:31:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     );
     //Establishes the connection
     echo 'This ', 'string ', 'was ', 'made ', 'with multiple parameters.', chr(10);
-    echo 'This ' , 'string ' , 'was ' , 'made ' , 'with concatenation.' , "\n";
+    echo 'This ' . 'string ' . 'was ' . 'made ' . 'with concatenation.' . "\n";
     $conn = sqlsrv_connect($serverName, $connectionOptions);
     $tsql= "SELECT * FROM users";
     $getResults= sqlsrv_query($conn, $tsql);
