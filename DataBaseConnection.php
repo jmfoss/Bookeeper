0732d8b579cf474e806406604d669986825ac8f3
Joshua Foss <jmfoss98@gmail.com>
2019-11-19 13:58:49 -0500

    Create DataBaseConnection.php

---
+++ DataBaseConnection.php
@@ -0,0 +1,24 @@
+<php?
+echo "<textarea name='mydata'>\n";
+    echo htmlspecialchars($data)."\n";
+    echo "</textarea>";
+
+    $serverName = "bookeeper.database.windows.net";
+    $connectionOptions = array(
+        "Database" => "Bookeeper",
+        "Uid" => "jmfoss",
+        "PWD" => "Mikito98"
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT * FROM users";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table<br>" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) 
+    {       
+       echo $row['userID'] . " " . $row['username'] . " " . $row['password'] . " " . date_format($row['join_date'],"Y/m/d H:i:s") . "\n";
+    }
+    sqlsrv_free_stmt($getResults);
+?>
