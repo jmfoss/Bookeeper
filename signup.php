94d3eefe82c36e033509bceb3abc8e90895fa39d
Joshua Foss <jmfoss98@gmail.com>
2019-11-20 14:02:57 -0500

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,38 @@
+<?php
+  $username = trim($_POST["username"]);
+  $password = trim($_POST["password"]);
+  $exists = 0;
+  $strength = 0;
+  $params = array(   
+                 array($username, SQLSRV_PARAM_IN),  
+                 array($exists, SQLSRV_PARAM_OUT)  
+               ); 
+  $sql = "EXEC checkUsername @username = ?";
+  $stmt = sqlsrv_query($conn, $sql, $params);
+  if(!$stmt)  
+  {  
+     die( print_r( sqlsrv_errors(), true));  
+  }
+  sqlsrv_free_stmt( $stmt);
+  if($exists)
+  {
+    echo "This user name is taken";
+  }
+  else
+  {
+    echo "This user name is not taken";
+  }
+  $params = array(   
+                 array($password, SQLSRV_PARAM_IN),  
+                 array($stength, SQLSRV_PARAM_OUT)  
+               );
+  $sql = "EXEC checkPassword @password = ?";
+  $stmt = sqlsrv_query($conn, $sql, $params);
+  if(!$stmt)  
+  {  
+     die( print_r( sqlsrv_errors(), true));  
+  }
+  sqlsrv_free_stmt( $stmt);
+  echo $stength;
+?>
+ 
