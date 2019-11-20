46297e43d4d7b9e9868aa2c2474af292076542b6
Joshua Foss <jmfoss98@gmail.com>
2019-11-19 22:33:02 -0500

    Update dbm_main.php

--- dbm_main.php
+++ dbm_main.php
@@ -4,7 +4,7 @@
 
 <!-- Main page -->
 <?php
-echo "hi";     
+    
 session_start();
      $serverName = "bookeeper.database.windows.net";
      $connectionOptions = array( "Bookeeper", "jmfoss", "Mikito98" );
@@ -70,6 +70,7 @@ session_start();
               <td> <label style = "margin:10px; padding:10px"> Username: </label> <input id="ip2" type="text" name="username" style = "margin:10px; padding:2px"> </td>
               <td> <label style = "margin:10px; padding:10px"> Password: </label> <input id="ip2" type="text" name="password" style = "margin:10px; padding:2px"> </td>
               <td> <label style = "margin:10px; padding:10px"> <input type="submit" name="submit" /> </td>
+                   <?php echo "hi"; ?>
           </form>
         </tr>
     </table>
