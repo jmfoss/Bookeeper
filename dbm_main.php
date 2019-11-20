1d8a5bc5d8fb927e0a11dd4f714c376425d2e316
Joshua Foss <jmfoss98@gmail.com>
2019-11-19 20:46:32 -0500

    Update dbm_main.php

--- dbm_main.php
+++ dbm_main.php
@@ -50,7 +50,6 @@ session_start();
              <form>
                <td> <label style = "margin:10px; padding:10px"> Username: </label> <input id="ip2" type="text" name="username" style = "margin:10px; padding:2px"> </td>
                <td> <label style = "margin:10px; padding:10px"> Password: </label> <input id="ip2" type="text" name="password" style = "margin:10px; padding:2px"> </td>
-               <td> <label style = "margin:10px; padding:10px"> <input type="submit" value="Login">   </td>
              </form>
         </tr>
     </table>
