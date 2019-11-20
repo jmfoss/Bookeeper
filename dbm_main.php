38cdd6b565e959469e0fb533ca360eedd5e80756
Joshua Foss <jmfoss98@gmail.com>
2019-11-20 10:45:17 -0500

    Update login button

--- dbm_main.php
+++ dbm_main.php
@@ -68,7 +68,7 @@ session_start();
           <form action="" method="post">
               <td> <label style = "margin:10px; padding:10px"> Username: </label> <input id="ip2" type="text" name="username" value="<?php echo htmlspecialchars($username); ?>" style = "margin:10px; padding:2px"> </td>
               <td> <label style = "margin:10px; padding:10px"> Password: </label> <input id="ip2" type="text" name="password" value="<?php echo htmlspecialchars($password); ?>" style = "margin:10px; padding:2px"> </td>
-              <td> <label style = "margin:10px; padding:10px"> <input type="submit" name="submit" value="Login" /> </td>
+              <td> <label style = "margin:10px; padding:10px"> <input type="submit" name="submit" value="Login" style = "margin:10px; padding:2px"/> </td>
           </form>
         </tr>
     </table>
