274dfbe6fd68185cfcf03a6069103c6add39dad9
Joshua Foss <jmfoss98@gmail.com>
2019-11-20 15:47:07 -0500

    Update dbm_main.php

--- dbm_main.php
+++ dbm_main.php
@@ -5,46 +5,7 @@
 <!-- Main page -->
 <?php   
 session_start();
-if ($_SERVER["REQUEST_METHOD"] == "POST")
-{
-  require_once "config.php";
-  $username = trim($_POST["username"]);
-  $password = trim($_POST["password"]);
-  $exists = 0;
-  $strength = 0;
-  $strengthword = "";
-  $params = array(   
-                 array(&$exists, SQLSRV_PARAM_OUT), 
-                 array($username, SQLSRV_PARAM_IN),  
-               ); 
-  $sql = "EXEC ?=checkUsername @username = ?";
-  $stmt = sqlsrv_query($conn, $sql, $params);
-  if(!$stmt)  
-  { 
-     die( print_r( sqlsrv_errors(), true));  
-  }
-  sqlsrv_free_stmt( $stmt);
-  if($exists)
-  {
-    echo "This user name is taken";
-  }
-  else
-  {
-    echo "This user name is not taken";
-  }
-  $params = array(   
-                 array(&$strength, SQLSRV_PARAM_OUT),
-                 array($password, SQLSRV_PARAM_IN), 
-               );
-  $sql = "EXEC ?=checkPassword @password = ?";
-  $stmt = sqlsrv_query($conn, $sql, $params);
-  if(!$stmt)  
-  {  
-     die( print_r( sqlsrv_errors(), true));  
-  }
-  sqlsrv_free_stmt( $stmt);
-  echo $strength
-}
+
 ?>
 
 
