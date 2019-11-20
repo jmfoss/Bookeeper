92b1e685c4bdaffad31a00713bc7e5ed78dce471
Joshua Foss <jmfoss98@gmail.com>
2019-11-20 15:46:08 -0500

    Update dbm_main.php

--- dbm_main.php
+++ dbm_main.php
@@ -86,7 +86,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     </p>
     <table align = "center">
         <tr>
-          <form action="" method="post">
+          <form action="signup.php" method="post">
               <td> <label style = "margin:10px; padding:10px"> Username: </label> <input id="ip2" type="text" name="username" style = "margin:10px; padding:2px"> </td>
               <td> <label style = "margin:10px; padding:10px"> Password: </label> <input id="ip2" type="text" name="password" style = "margin:10px; padding:2px"> </td>
               <td> <label style = "margin:10px; padding:10px"> <input type="submit" name="submit" value="Sign Up" style = "margin:10px; padding:2px"/> </td>
