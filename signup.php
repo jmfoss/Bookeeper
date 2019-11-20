655fdf548218f691541cc6e4bda73b728d625988
Joshua Foss <jmfoss98@gmail.com>
2019-11-20 15:28:56 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -26,7 +26,7 @@
   }
   $params = array(   
                  array(&$strength, SQLSRV_PARAM_OUT),
-                 array(&$strengthword, SQLSRV_PARAM_OUT)
+                 array(&$strengthword, SQLSRV_PARAM_OUT),
                  array($password, SQLSRV_PARAM_IN), 
                );
   $sql = "EXEC ?=checkPassword @password = ?";
