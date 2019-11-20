b7a7c31822ebc4a0b1e12da4dbcecedb24c44799
Joshua Foss <jmfoss98@gmail.com>
2019-11-20 11:06:53 -0500

    Update dbm_main.php

--- dbm_main.php
+++ dbm_main.php
@@ -4,26 +4,7 @@
 
 <!-- Main page -->
 <?php   
-#require_once "config.php";
 echo "Testing";
-#session_start();
-     #$serverName = "bookeeper.database.windows.net";
-     #$connectionOptions = array( "Bookeeper", "jmfoss", "Mikito98" );
-     #if ( isset( $_POST['submit'] )
-        # {
-             # $conn = sqlsrv_connect($serverName, $connectionOptions);
-             # $username = $_REQUEST['username'];
-             # $password = $_REQUEST['password'];
-             # if (empty($username))
-                   echo "PLease input a username";
-             # $sqlQuery= "SELECT userID FROM users WHERE username = $username";
-             # $getResults= sqlsrv_query($conn, $sqlQuery);
-             # if ($getResults == FALSE)
-             #     echo (sqlsrv_errors());
-             # if(sqlsrv_num_rows($getResults) == 1)
-                  echo "This username is taken";
-             # sqlsrv_free_stmt($getResults);
-        # }
 ?>
 <!DOCTYPE html>
 
@@ -67,7 +48,7 @@ echo "Testing";
         <tr>
           <form action="" method="post">
               <td> <label style = "margin:10px; padding:10px"> Username: </label> <input id="ip2" type="text" name="username" value="<?php echo "Test"; ?>" style = "margin:10px; padding:2px"> </td>
-              <td> <label style = "margin:10px; padding:10px"> Password: </label> <input id="ip2" type="text" name="password" value="<?php echo "test"; ?>" style = "margin:10px; padding:2px"> </td>
+              <td> <label style = "margin:10px; padding:10px"> Password: </label> <input id="ip2" type="text" name="password" value="<?php echo "Test"; ?>" style = "margin:10px; padding:2px"> </td>
               <td> <label style = "margin:10px; padding:10px"> <input type="submit" name="submit" value="Login" style = "margin:10px; padding:2px"/> </td>
           </form>
         </tr>
