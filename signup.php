2846aeff45d82b0bd5d39e37aa6d080f75f7d48f
jmtucci <36246394+jmtucci@users.noreply.github.com>
2019-11-21 12:27:48 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,3 +1,23 @@
+
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px"> </div>
+    <div class="topnav" id="myTopnav">
+      <a href="dbm_main.html" class="active"> Home </a>
+      <a href="dbm_searchbooks.html"> Search Books </a>
+      <a href="dbm_addbooks.html"> Add Books </a>
+      <a href="dbm_library.html"> My Library </a>
+      <!-- <a href="uploadDoc.html"> Upload Documents </a> -->
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"> </i>
+      </a>
+    </div>
+    
 <?php
   require_once "config.php";
   $username = trim($_POST["username"]);
@@ -37,3 +57,4 @@
   echo $strength;
 ?>
  
+</html>
