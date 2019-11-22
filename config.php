bf0ee0bd7d86164d350486347f097832ba26234f
Joshua Foss <jmfoss98@gmail.com>
2019-11-22 12:17:43 -0500

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,7 @@ $serverName = "bookeeper.database.windows.net"; //serverName\instanceName
 $connectionInfo = array( "Database"=>"bookeeper", "UID"=>"jmfoss", "PWD"=>"Mikito98");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
  
-if( $conn ) {
-     echo "Connection established.<br />";
-}else{
-     echo "Connection could not be established.<br />";
+if( !$conn ) {
      die( print_r( sqlsrv_errors(), true));
 }
 ?>
