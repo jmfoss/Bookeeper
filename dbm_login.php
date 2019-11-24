e7a573196ad0e12727e49aa53bf3e3075f6c15b2
Joshua Foss <jmfoss98@gmail.com>
2019-11-24 12:09:47 -0500

    Update dbm_login.php

--- dbm_login.php
+++ dbm_login.php
@@ -43,7 +43,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     if(empty($username_err) && empty($password_err))
     {
         // Prepare a select statement
-        $sql = "SELECT userID, username, password FROM users WHERE @username = ?";
+        $sql = "SELECT userID, username, password FROM users WHERE username = ?";
         $stmt = sqlsrv_query($conn, $sql, array($username));
         if($stmt != false)
         {              
@@ -75,7 +75,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             {
 
                 // Display an error message if username doesn't exist
-                $username_err = "No account found with that username.";
+                $username_err = "No account found with that username." + sqlsrv_errors();
             }
         } 
         else
