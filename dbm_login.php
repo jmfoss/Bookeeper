19c2478502a165b0a70ffe960d3e4105122c882b
Joshua Foss <jmfoss98@gmail.com>
2019-11-24 11:42:02 -0500

    Update dbm_login.php

--- dbm_login.php
+++ dbm_login.php
@@ -1,81 +1,90 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
+{
     header("location: dbm_main.php");
     exit;
 }
- 
+
 // Include config file
 require_once "config.php";
- 
+
 // Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = "";
- 
+
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
     // Check if username is empty
-    if(empty(trim($_POST["username"]))){
+    if(empty(trim($_POST["username"])))
+    {
         $username_err = "Please enter username.";
-    } else{
+    } 
+    else
+    {
         $username = trim($_POST["username"]);
     }
-    
+
     // Check if password is empty
-    if(empty(trim($_POST["password"]))){
+    if(empty(trim($_POST["password"])))
+    {
         $password_err = "Please enter your password.";
-    } else{
+    } 
+    else
+    {
         $password = trim($_POST["password"]);
     }
-    
+
     // Validate credentials
-    if(empty($username_err) && empty($password_err)){
+    if(empty($username_err) && empty($password_err))
+    {
         // Prepare a select statement
         $sql = "SELECT userid, username, password FROM users WHERE username = ?";
-        
+
         if($stmt = sqlsrv_query($conn, $sql, $username))
         {              
-                // Check if username exists, if yes then verify password
-                if(sqlsrv_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    $array = [];
-                    sqlsrv_fetch_array($stmt, $array);
-                    if(password_verify($password, $array["password"])){
-                        // Password is correct, so start a new session
-                        session_start();
+            // Check if username exists, if yes then verify password
+            if(sqlsrv_num_rows($stmt) == 1)
+            {                    
+                // Bind result variables
+                $array = [];
+                sqlsrv_fetch_array($stmt, $array);
+                if(password_verify($password, $array["password"]))
+                {
+                    // Password is correct, so start a new session
+                    session_start();
 
-                        // Store data in session variables
-                        $_SESSION["loggedin"] = true;
-                        $_SESSION["id"] = $array["userID"];
-                        $_SESSION["username"] = $array["username"];                            
+                    // Store data in session variables
+                    $_SESSION["loggedin"] = true;
+                    $_SESSION["id"] = $array["userID"];
+                    $_SESSION["username"] = $array["username"];                            
 
-                        // Redirect user to welcome page
-                        header("location: dbm_main.php");
-                    } 
-                    else
-                    {
-                        // Display an error message if password is not valid
-                        $password_err = "The password you entered was not valid.";
-                    }
+                    // Redirect user to welcome page
+                    header("location: dbm_main.php");
                 } 
                 else
                 {
-                    // Display an error message if username doesn't exist
-                    $username_err = "No account found with that username.";
+                    // Display an error message if password is not valid
+                    $password_err = "The password you entered was not valid.";
                 }
-         } 
-         else
-         {
-                echo "Oops! Something went wrong. Please try again later.";
-         }
-     }
-        
+            } 
+            else
+            {
+                // Display an error message if username doesn't exist
+                $username_err = "No account found with that username.";
+            }
+        } 
+        else
+        {
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+
     }
-    
+
 }
 ?>
  
