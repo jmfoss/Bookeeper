803f87eb00e1eeb414d4b53875b660ae9361d96e
Joshua Foss <jmfoss98@gmail.com>
2019-11-24 15:30:02 -0500

    Update dbm_addbook.php

--- dbm_addbook.php
+++ dbm_addbook.php
@@ -114,15 +114,6 @@
                 <option value="currentlyreading"> Currently Reading </option>
 			</select>
         </div>
-	    <head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
               <div class = "wrapper" id="questions">
                 <table>
                     <tr>
