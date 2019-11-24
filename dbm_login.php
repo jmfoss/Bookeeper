9ad0d0e2ba329c44de930fb8fccb6398deb37ded
Joshua Foss <jmfoss98@gmail.com>
2019-11-24 11:20:55 -0500

    Update dbm_login.php

--- dbm_login.php
+++ dbm_login.php
@@ -1,126 +1,124 @@
-<!-- Joseph Santucci, Joshua FOss
-     Database Management
-     28 October 2019 -->
-
-<!-- Main page -->
-<?php  
+<?php
+// Initialize the session
 session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+ 
+// Include config file
 require_once "config.php";
-$username = "";
-$password = "";
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-
-  $username = trim($_POST["username"]);
-  $password = trim($_POST["password"]);
-  $exists = 0;
-  $strength = " ";
-  $usernameMSG = "";
-  $passwordMSG = "";
-  $passwordValid = false;
-  $usernameValid = false;
-  $params = array(   
-                 array(&$exists, SQLSRV_PARAM_OUT), 
-                 array($username, SQLSRV_PARAM_IN),  
-               ); 
-  $sql = "EXEC ?=checkUsername @username = ?";
-  $stmt = sqlsrv_query($conn, $sql, $params);
-  if(!$stmt)  
-  { 
-     die( print_r( sqlsrv_errors(), true));  
-  }
-  sqlsrv_free_stmt( $stmt);
-  if($exists)
-  {
-    $usernameValid = false;
-    $usernameMSG == "This user name is taken";
-  }
-  else
-  {
-    $usernameValid = true;
-  }
-  $params = array(   
-                  array($password, SQLSRV_PARAM_IN),
-                  array(&$strength, SQLSRV_PARAM_OUT),
-               );
-  $sql = "EXEC checkPassword @password = ?, @OutString = ?";
-  $stmt = sqlsrv_query($conn, $sql, $params);
-  if(!$stmt)  
-  {  
-    die( print_r( sqlsrv_errors(), true));  
-  }
-  sqlsrv_free_stmt( $stmt);
-  If ($strength == "STRONG")
-  {
-    $passwordValid = true;
-    $passwordMSG = "This is a strong password";
-  }
-  else if ($strength == "MEDIUM")
-  {
-    $passwordValid = true;
-    $passwordMSG = "This is a medium password";
-  }
-  else if ($strength == "WEAK")
-  {
-    $passwordValid = false;
-    $passwordMSG = "This is password is too weak";
-  }
-  else
-  {
-    $passwordValid = false;
-    $passwordMSG = "Passwords cannot contain spaces";
-  }
-  
-  if ($passwordValid && $usernameValid)
-  {
-    $params = array(   
-                  array($_SESSION["userID"], SQLSRV_PARAM_OUT),         
-                  array($username, SQLSRV_PARAM_IN),
-                  array($password, SQLSRV_PARAM_IN),
-               );
-    $sql = "EXEC ?=addUser @username = ?, @password = ?";
-    $stmt = sqlsrv_query($conn, $sql, $params);
-    header("Location: dbm_main.php");
-    exit();
-  }
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: welcome.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "No account found with that username.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
 }
 ?>
+ 
 <!DOCTYPE html>
-
-<html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
-    <body>
-        <div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px; margin-bottom: 100px"> </div>
-        
-        <form action = "dbm_login.php" method = "post">
-            <table align = "center" style = "margin-bottom: 40px; border: 1px " >
-                <tr>
-                    <td> New? Sign up!</td>
-                </tr>
-                <tr>
-                    <td> <label style = "margin:10px; padding:10px"> Username: </label> <input id="ip2" type="text" name="username" value = "<? echo $username; ?>" style = "margin:10px; padding:2px"> </td>
-                    <td> <label style = "margin:10px; padding:10px"> Password: </label> <input id="ip2" type="text" name="password" value = "<? echo $password; ?>" style = "margin:10px; padding:2px"> </td>
-                    <td> <input type = "submit" value = "Sign Up" style = "margin:20px;margin-top:10px"/> </td>
-                </tr>
-            </table>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
         </form>
-        <hr class ="striped-border">
-        <br>
-        <form action = "" method = "post">
-            <table align = "center">
-                <tr>
-                    <td> Already have an account? Log In!</td>
-                </tr>
-                <tr>
-                    <td> <label style = "margin:10px; padding:10px"> Username: </label> <input id="ip2" type="text" name="username" value = "<? echo $username; ?>" style = "margin:10px; padding:2px"> </td>
-                    <td> <label style = "margin:10px; padding:10px"> Password: </label> <input id="ip2" type="text" name="password" value = "<? echo $password; ?>" style = "margin:10px; padding:2px"> </td>
-                    <td> <input type = "submit"  value = "Log In" style = "margin:20px;margin-top:10px"/> </td>
-                </tr>
-            </table>
-         </form>
-    </body>
+    </div>    
+</body>
 </html>
