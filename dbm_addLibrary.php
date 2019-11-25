ea09bf136c70d41a4aefc73853672fa1152e7393
Joshua Foss <jmfoss98@gmail.com>
2019-11-25 17:38:16 -0500

    Update dbm_addLibrary.php

--- dbm_addLibrary.php
+++ dbm_addLibrary.php
@@ -4,6 +4,53 @@
 
 <!-- Main page -->
 <?php
-echo "hi";                 
+// Initialize the session
+session_start();
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
+{
+    header("location: dbm_main.php");
+    exit;
+}
+
+// Include config file
+require_once "config.php";
+$list = $book = "";
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+     //Options: read, reading, future
+     $list = trim($_POST["list"]);
+     $book = trim($_POST["book"]);
+     &sql = "EXEC addToList @userID = ?, @listName = ?, @title = ?";
+     $params = array(   
+                     array($_SESSION["userID"], SQLSRV_PARAM_IN), 
+                     array($list, SQLSRV_PARAM_IN),
+                     array($book, SQLSRV_PARAM_IN)
+                     ); 
+     $stmt = sqlsrv_query($conn, $sql, $params); 
+}
 ?>
 
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px"> </div>
+    <div class="topnav" id="myTopnav">
+      <a href="dbm_main.php"> Home </a>
+      <a href="dbm_searchbooks.html"> Search Books </a>
+      <a href="dbm_addbooks.html"> Add Books </a>
+      <a href="dbm_library.html" class="active"> My Library </a>
+      <!-- <a href="uploadDoc.html"> Upload Documents </a> -->
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"> </i>
+      </a>
+    </div>
+
+    
+  </body>
