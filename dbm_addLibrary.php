4b4bf578a27bcbe243a1ee248af864c45bc7e4d6
Joshua Foss <jmfoss98@gmail.com>
2019-11-25 17:41:35 -0500

    Update dbm_addLibrary.php

--- dbm_addLibrary.php
+++ dbm_addLibrary.php
@@ -16,6 +16,29 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
 // Include config file
 require_once "config.php";
 $list = $book = "";
+$options = array
+(
+    'hostname' => SOLR_SERVER_HOSTNAME,
+    'login'    => SOLR_SERVER_USERNAME,
+    'password' => SOLR_SERVER_PASSWORD,
+    'port'     => SOLR_SERVER_PORT,
+);
+$client = new SolrClient($options);
+
+$query = new SolrQuery();
+
+$query->setQuery('*:*');
+
+$query->setStart(0);
+
+$query->setRows(50);
+
+$query_response = $client->query($query);
+
+$response = $query_response->getResponse();
+
+print_r($response);
+
 ?>
 
 <!DOCTYPE html>
