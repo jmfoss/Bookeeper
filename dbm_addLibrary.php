b8d9c491cd5db0647b20113cb3460f1d376d37e0
Joshua Foss <jmfoss98@gmail.com>
2019-11-25 17:20:19 -0500

    Update dbm_addLibrary.php

--- dbm_addLibrary.php
+++ dbm_addLibrary.php
@@ -38,26 +38,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         }                   
 ?>
 <!DOCTYPE html>
+<html lang="en">
 
-<html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="style.css">
-  </head>
-     
-  <body>
-    <div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px"> </div>
-    <div class="topnav" id="myTopnav">
-      <a href="dbm_main.php"> Home </a>
-      <a href="dbm_searchbooks.html"> Search Books </a>
-      <a href="dbm_addbooks.html"> Add Books </a>
-      <a href="dbm_library.html" class="active"> My Library </a>
-      <!-- <a href="uploadDoc.html"> Upload Documents </a> -->
-      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-        <i class="fa fa-bars"> </i>
-      </a>
-    </div>
+<head>
     <meta http-equiv="Content-Language" content="en-us">
     <title>PHP MySQL Typeahead Autocomplete</title>
     <meta charset="utf-8">
@@ -113,7 +96,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 
         })
     </script>
-
+</head>
 
 <body>
     <div class="content">
@@ -123,6 +106,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             <input type="text" name="city" size="30" class="city" placeholder="Please Enter City or ZIP code">
         </form>
     </div>
-     </body>
-  </body>
+</body>
+
 </html>
