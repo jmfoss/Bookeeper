3326bde8c13d7f32bd80ad7c1a55c2ae46767be7
Joshua Foss <jmfoss98@gmail.com>
2019-11-25 17:36:14 -0500

    Update dbm_addLibrary.php

--- dbm_addLibrary.php
+++ dbm_addLibrary.php
@@ -37,27 +37,4 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             echo "Oops! Something went wrong. Please try again later.";
         }                   
 ?>
-<!DOCTYPE html>
 
-<html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="style.css">
-  </head>
-  <body>
-    <div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px"> </div>
-    <div class="topnav" id="myTopnav">
-      <a href="dbm_main.php"> Home </a>
-      <a href="dbm_searchbooks.html"> Search Books </a>
-      <a href="dbm_addbooks.html"> Add Books </a>
-      <a href="dbm_library.html" class="active"> My Library </a>
-      <!-- <a href="uploadDoc.html"> Upload Documents </a> -->
-      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-        <i class="fa fa-bars"> </i>
-      </a>
-    </div>
-
-    
-  </body>
-</html>
