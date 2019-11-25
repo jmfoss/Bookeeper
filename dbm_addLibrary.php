4938e8385a49da6187f87fc63a902d590ccb00ad
Joshua Foss <jmfoss98@gmail.com>
2019-11-25 17:19:37 -0500

    Update dbm_addLibrary.php

--- dbm_addLibrary.php
+++ dbm_addLibrary.php
@@ -45,6 +45,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
   </head>
+     
   <body>
     <div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px"> </div>
     <div class="topnav" id="myTopnav">
@@ -57,7 +58,71 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         <i class="fa fa-bars"> </i>
       </a>
     </div>
+    <meta http-equiv="Content-Language" content="en-us">
+    <title>PHP MySQL Typeahead Autocomplete</title>
+    <meta charset="utf-8">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="//netsh.pp.ua/upwork-demo/1/js/typeahead.js"></script>
+    <style>
+        h1 {
+            font-size: 20px;
+            color: #111;
+        }
 
-    
+        .content {
+            width: 80%;
+            margin: 0 auto;
+            margin-top: 50px;
+        }
+
+        .tt-hint,
+        .city {
+            border: 2px solid #CCCCCC;
+            border-radius: 8px 8px 8px 8px;
+            font-size: 24px;
+            height: 45px;
+            line-height: 30px;
+            outline: medium none;
+            padding: 8px 12px;
+            width: 400px;
+        }
+
+        .tt-dropdown-menu {
+            width: 400px;
+            margin-top: 5px;
+            padding: 8px 12px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border: 1px solid rgba(0, 0, 0, 0.2);
+            border-radius: 8px 8px 8px 8px;
+            font-size: 18px;
+            color: #111;
+            background-color: #F1F1F1;
+        }
+    </style>
+    <script>
+        $(document).ready(function() {
+
+            $('input.city').typeahead({
+                name: 'city',
+                remote: 'city.php?query=%QUERY'
+
+            });
+
+        })
+    </script>
+
+
+<body>
+    <div class="content">
+
+        <form>
+            <h1>Try it yourself</h1>
+            <input type="text" name="city" size="30" class="city" placeholder="Please Enter City or ZIP code">
+        </form>
+    </div>
+     </body>
   </body>
 </html>
