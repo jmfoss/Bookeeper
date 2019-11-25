62c8ae1571ff0b187dd66ce46b3bc01bceaec170
Joshua Foss <jmfoss98@gmail.com>
2019-11-25 17:30:11 -0500

    Update config.php

--- config.php
+++ config.php
@@ -7,4 +7,13 @@ $conn = sqlsrv_connect( $serverName, $connectionInfo);
 if( !$conn ) {
      die( print_r( sqlsrv_errors(), true));
 }
+
+$solr = array(
+    'endpoint' => array(
+        'localhost' => array(
+            'host' => '127.0.0.1', 'port' => '8983', 'path' => '/solr/#/bookeeper'
+        )
+    )
+);
+$client = new Solarium\Client($config);
 ?>
