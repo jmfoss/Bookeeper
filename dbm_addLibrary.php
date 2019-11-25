cb93e090f17bd9f96f53b334e536a6f84509b4cd
Joshua Foss <jmfoss98@gmail.com>
2019-11-25 18:37:27 -0500

    Update dbm_addLibrary.php

--- dbm_addLibrary.php
+++ dbm_addLibrary.php
@@ -37,6 +37,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
           {
                echo "This book is already in the your list.";
           }
+	  else
+	  {
+		echo "$title was added to $list";
+	  }
      }
 }
 
