a2675b0ee54f827d4b4ef6c41909db0ad7bec214
Joshua Foss <jmfoss98@gmail.com>
2019-11-25 17:37:49 -0500

    Update dbm_addLibrary.php

--- dbm_addLibrary.php
+++ dbm_addLibrary.php
@@ -4,29 +4,6 @@
 
 <!-- Main page -->
 <?php
-// Initialize the session
-session_start();
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
-{
-    header("location: dbm_main.php");
-    exit;
-}
-
-// Include config file
-require_once "config.php";
-$list = $book = "";
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-     //Options: read, reading, future
-     $list = trim($_POST["list"]);
-     $book = trim($_POST["book"]);
-     &sql = "EXEC addToList @userID = ?, @listName = ?, @title = ?";
-     $params = array(   
-                     array($_SESSION["userID"], SQLSRV_PARAM_IN), 
-                     array($list, SQLSRV_PARAM_IN),
-                     array($book, SQLSRV_PARAM_IN)
-                     ); 
-     $stmt = sqlsrv_query($conn, $sql, $params);                  
+echo "hi";                 
 ?>
 
