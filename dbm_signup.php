31ba0293fde5146e9fb81de0570a035dc0f942a6
jmtucci <36246394+jmtucci@users.noreply.github.com>
2019-11-25 20:04:39 -0500

    Update dbm_signup.php

--- dbm_signup.php
+++ dbm_signup.php
@@ -120,8 +120,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 <body>
     <div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px"> </div>
  
-    <div class="wrapper" align = "center">
-        <h2 align = "center">Sign Up</h2>
+    <div class="wrapper" postion = "absolute">
+        <h2>Sign Up</h2>
+        <br>
         <p>Please fill this form to create an account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
