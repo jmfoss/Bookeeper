ef0649a0d694b662498076a8df21c582636a51e3
Joshua Foss <jmfoss98@gmail.com>
2019-11-25 19:06:57 -0500

    Update dbm_addLibrary.php

--- dbm_addLibrary.php
+++ dbm_addLibrary.php
@@ -69,6 +69,76 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script  type="text/javascript" src="/examples/js/typeahead/0.11.1/typeahead.bundle.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function(){
+	    // Sonstructs the suggestion engine
+	    var titles = new Bloodhound({
+		datumTokenizer: Bloodhound.tokenizers.whitespace,
+		queryTokenizer: Bloodhound.tokenizers.whitespace,
+		// The url points to a json file that contains an array of country names
+		prefetch: '/examples/data/countries.json'
+	    });
+
+	    // Initializing the typeahead with remote dataset
+	    $('.typeahead').typeahead(null, {
+		name: 'title',
+		source: titles,
+		limit: 10 /* Specify maximum number of suggestions to be displayed */
+	    });
+	});  
+	</script>
+	<style type="text/css">
+	.bs-example {
+		font-family: sans-serif;
+		position: relative;
+		margin: 100px;
+	}
+	.typeahead, .tt-query, .tt-hint {
+		border: 2px solid #CCCCCC;
+		border-radius: 8px;
+		font-size: 22px; /* Set input font size */
+		height: 30px;
+		line-height: 30px;
+		outline: medium none;
+		padding: 8px 12px;
+		width: 396px;
+	}
+	.typeahead {
+		background-color: #FFFFFF;
+	}
+	.typeahead:focus {
+		border: 2px solid #0097CF;
+	}
+	.tt-query {
+		box-shadow: 0 1px 1px rgba(0, 0, 0, 0.075) inset;
+	}
+	.tt-hint {
+		color: #999999;
+	}
+	.tt-menu {
+		background-color: #FFFFFF;
+		border: 1px solid rgba(0, 0, 0, 0.2);
+		border-radius: 8px;
+		box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+		margin-top: 12px;
+		padding: 8px 0;
+		width: 422px;
+	}
+	.tt-suggestion {
+		font-size: 22px;  /* Set suggestion dropdown font size */
+		padding: 3px 20px;
+	}
+	.tt-suggestion:hover {
+		cursor: pointer;
+		background-color: #0097CF;
+		color: #FFFFFF;
+	}
+	.tt-suggestion p {
+		margin: 0;
+	}
+</style>
   </head>
   <body>
     <div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px"> </div>
@@ -77,8 +147,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
       <a href="dbm_searchbooks.html"> Search Books </a>
       <a href="dbm_addbooks.html"> Add Books </a>
       <a href="dbm_library.html" class="active"> My Library </a>
-      <!-- <a href="uploadDoc.html"> Upload Documents </a> -->
-      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
         <i class="fa fa-bars"> </i>
       </a>
 <script>
@@ -114,7 +182,7 @@ $(document).ready(function(){
 				</select>
                         <div class="form-group <?php echo (!empty($title_err)) ? 'has-error' : ''; ?>">
 			  <td> <label style = "margin:10px; padding:10px"> Title </label> </td>
-                          <td> <input id="ip2" type="text" name="title" value="<?php echo $title; ?>" style = "margin:10px; padding:2px"> </td>
+                          <td> <input id="ip2" type="text" class="typeahead tt-query" autocomplete="off" spellcheck="false" name="title" value="<?php echo $title; ?>" style = "margin:10px; padding:2px"> </td>
 			  <span class="help-block"><?php echo $title_err; ?></span>
 		        </div>  
                 </table>
