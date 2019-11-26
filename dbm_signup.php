e8a3808c271b914d08e76c0fe372cdf51a5eae77
jmtucci <36246394+jmtucci@users.noreply.github.com>
2019-11-25 20:05:17 -0500

    Update dbm_signup.php

--- dbm_signup.php
+++ dbm_signup.php
@@ -120,7 +120,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 <body>
     <div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px"> </div>
  
-    <div class="wrapper" postion = "absolute">
+    <div class="wrapper">
         <h2>Sign Up</h2>
         <br>
         <p>Please fill this form to create an account.</p>
