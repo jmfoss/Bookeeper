dcd58f333557ecb525d401a126a14798e80a4f44
Joshua Foss <jmfoss98@gmail.com>
2019-11-26 23:25:03 -0500

    Update dbm_delete.php

--- dbm_delete.php
+++ dbm_delete.php
@@ -1,1 +1,20 @@
-
+<?php
+// Initialize the session
+session_start();
+require_once "config.php";
+$sql = "DELETE FROM users WHERE userID = ?";
+$stmt = sqlsrv_query($conn, $sql, array($_SESSION["userID"]));
+if(!$stmt)
+{  
+  echo "Whoa, something went wrong!";
+}
+// Unset all of the session variables
+$_SESSION = array();
+ 
+// Destroy the session.
+session_destroy();
+ 
+// Redirect to login page
+header("location: dbm_login.php");
+exit;
+?>
