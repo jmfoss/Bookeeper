3250db0da4f90ff1d73e74191b1f53441f91f91f
Joshua Foss <jmfoss98@gmail.com>
2019-11-27 01:31:56 -0500

    Update dbm_login.php

--- dbm_login.php
+++ dbm_login.php
@@ -51,7 +51,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             // Check if username exists, if yes then verify password
             if(!empty($row))
             {                    
-                echo $row["password"];
+                
                 if(password_verify($password, $row["password"]))
                 {
                     // Password is correct, so start a new session
@@ -67,7 +67,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
                 }
                 else
                 {
-                    $password_err = "The password you entered was not valid.";
+                    $password_err = "$row['password'] The password you entered was not valid.";
                 }
             } 
             else
