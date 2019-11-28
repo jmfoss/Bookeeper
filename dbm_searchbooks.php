b9d779eef4339692268342f37b4c281b2bce766a
Joshua Foss <jmfoss98@gmail.com>
2019-11-28 00:27:49 -0500

    Update dbm_searchbooks.php

--- dbm_searchbooks.php
+++ dbm_searchbooks.php
@@ -37,9 +37,10 @@ $query->addSort('price', $query::SORT_ASC);
 
 // this executes the query and returns the result
 $resultset = $client->select($query);
-/*
+
 // display the total number of documents found by solr
 echo 'NumFound: '.$resultset->getNumFound();
+/*
 // display the max score
 echo '<br>MaxScore: '.$resultset->getMaxScore();
 // show documents using the resultset iterator
