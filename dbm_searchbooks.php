d770cd3b4f1db62f06c8e296a0a9c9e79d835832
Joshua Foss <jmfoss98@gmail.com>
2019-12-01 14:47:32 -0500

    Update dbm_searchbooks.php

--- dbm_searchbooks.php
+++ dbm_searchbooks.php
@@ -6,6 +6,19 @@
 
 
 <?php
+$config = array(
+    'endpoint' => array(
+        'localhost' => array(
+            'host' => '104.230.35.171',
+            'port' => 8983,
+            'path' => '/',
+            'core' => 'bookeeper',
+            // For Solr Cloud you need to provide a collection instead of core:
+            // 'collection' => 'techproducts',
+        )
+    )
+);
+
 require(__DIR__.'/init.php');
 
 // check solarium version available
