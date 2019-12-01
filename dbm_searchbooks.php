5a3010c56c3df283a3fbbfa3f2b0f8dc86c5f48f
Joshua Foss <jmfoss98@gmail.com>
2019-12-01 15:33:45 -0500

    Update dbm_searchbooks.php

--- dbm_searchbooks.php
+++ dbm_searchbooks.php
@@ -21,25 +21,40 @@ $config = array(
 );
 require(__DIR__.'/init.php');
 
-// check solarium version available
-echo 'Solarium library version: ' . Solarium\Client::VERSION . ' - ';
+htmlHeader();
 
 // create a client instance
 $client = new Solarium\Client($config);
 
-// create a ping query
-$ping = $client->createPing();
+// get a select query instance
+$query = $client->createQuery($client::QUERY_SELECT);
 
-// execute the ping query
-try {
-    $result = $client->ping($ping);
-    echo 'Ping query successful';
-    echo '<br/><pre>';
-    var_dump($result->getData());
-    echo '</pre>';
-} catch (Solarium\Exception $e) {
-    echo 'Ping query failed';
-}        
+// this executes the query and returns the result
+$resultset = $client->execute($query);
+
+// display the total number of documents found by solr
+echo 'NumFound: '.$resultset->getNumFound();
+
+// show documents using the resultset iterator
+foreach ($resultset as $document) {
+
+    echo '<hr/><table>';
+
+    // the documents are also iterable, to get all fields
+    foreach ($document as $field => $value) {
+        // this converts multivalue fields to a comma-separated string
+        if (is_array($value)) {
+            $value = implode(', ', $value);
+        }
+
+        echo '<tr><th>' . $field . '</th><td>' . $value . '</td></tr>';
+    }
+
+    echo '</table>';
+}
+
+htmlFooter();
+    
 
 ?>
 
