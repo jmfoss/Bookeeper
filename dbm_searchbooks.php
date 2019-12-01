97f96a50b6c485a19e9f5bacb2ee29796e3e793e
Joshua Foss <jmfoss98@gmail.com>
2019-12-01 16:24:30 -0500

    Update dbm_searchbooks.php

--- dbm_searchbooks.php
+++ dbm_searchbooks.php
@@ -21,27 +21,36 @@ require(__DIR__.'/init.php');
 
 // create a client instance
 $client = new Solarium\Client($config);
+
 // get a suggester query instance
 $query = $client->createSuggester();
-$query->setQuery('c');
-$query->setDictionary('mySuggester');
-$query->setBuild(true);
+$query->setQuery('h'); //multiple terms
+$query->setDictionary('suggest');
+$query->setOnlyMorePopular(true);
 $query->setCount(10);
+$query->setCollate(true);
+
 // this executes the query and returns the result
 $resultset = $client->suggester($query);
+
 echo '<b>Query:</b> '.$query->getQuery().'<hr/>';
+
 // display results for each term
-foreach ($resultset as $dictionary => $terms) {
-    echo '<h3>' . $dictionary . '</h3>';
-    foreach ($terms as $term => $termResult) {
-        echo '<h4>' . $term . '</h4>';
-        echo 'NumFound: '.$termResult->getNumFound().'<br/>';
-        foreach ($termResult as $result) {
-            echo '- '.$result['term'].'<br/>';
-        }
+foreach ($resultset as $term => $termResult) {
+    echo '<h3>' . $term . '</h3>';
+    echo 'NumFound: '.$termResult->getNumFound().'<br/>';
+    echo 'StartOffset: '.$termResult->getStartOffset().'<br/>';
+    echo 'EndOffset: '.$termResult->getEndOffset().'<br/>';
+    echo 'Suggestions:<br/>';
+    foreach ($termResult as $result) {
+        echo '- '.$result.'<br/>';
     }
+
     echo '<hr/>';
 }
+
+// display collation
+echo 'Collation: '.$resultset->getCollation();
 ?>
 
 <!DOCTYPE html>
