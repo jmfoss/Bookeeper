b3e31910dc374338c07b5beab9c2f4ee42d60ff0
Joshua Foss <jmfoss98@gmail.com>
2019-12-01 15:26:24 -0500

    Update init.php

--- init.php
+++ init.php
@@ -4,7 +4,7 @@ ini_set('display_errors', true);
 if (file_exists($_SERVER['DOCUMENT_ROOT'] .'/config.php')) {
     require($_SERVER['DOCUMENT_ROOT'] .'/config.php');
 }
-require $config['autoload'] ?? __DIR__.'/../vendor/autoload.php';
+require $config['autoload'] ?? __DIR__.'/vendor/autoload.php';
 function htmlHeader()
 {
     echo '<html><head><title>Solarium examples</title></head><body><nav><a href="index.html">Back to Overview</a></nav><br><article>';
