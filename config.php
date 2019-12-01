8ff4e1d7087e81d5586c4878815e2a3b83e56c6b
Joshua Foss <jmfoss98@gmail.com>
2019-12-01 15:29:59 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,5 @@
 <?php
 
-$config = array(
-    'endpoint' => array(
-        'localhost' => array(
-            'host' => '104.230.35.171',
-            'port' => 8983,
-            'path' => '/',
-            'core' => 'bookeeper',
-            // For Solr Cloud you need to provide a collection instead of core:
-            // 'collection' => 'techproducts',
-        )
-    )
-);
-require(__DIR__.'/init.php');
-
 $serverName = "bookeeper.database.windows.net"; //serverName\instanceName
 $connectionInfo = array( "Database"=>"bookeeper", "UID"=>"jmfoss", "PWD"=>"Mikito98");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
