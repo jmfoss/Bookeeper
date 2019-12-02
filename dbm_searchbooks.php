560b20f24430a5a8434a045f4eeb8e73260acf44
Joshua Foss <jmfoss98@gmail.com>
2019-12-02 00:04:54 -0500

    Update dbm_searchbooks.php

--- dbm_searchbooks.php
+++ dbm_searchbooks.php
@@ -27,7 +27,6 @@ $userQuery = trim($_GET['query']);
 // get a suggester query instance
 $query = $client->createSuggester();
 $query->setQuery($userQuery);
-$suggestions = $resultset;
 // this executes the query and returns the result
 $resultset = $client->suggester($query);
 
@@ -169,76 +168,21 @@ th {
       <a href="dbm_addLibrary.php" > My Library </a>
       </a>
     </div>
+      </form>
         <form autocomplete="off" action = "" method = "post">
-
         </div>
-              <body>
-    	<h2>The form</h2>
-		    <form>
-			<input type="text" name="name" id="input" list="huge_list">Name
-			<datalist id="huge_list">
-			</datalist>
-			<br/>
-			<input type="submit">
-		    </form>
-		</body>
-          </form>
-<script>
-window.addEventListener("load", function(){
+              <div class = "autocomplete" id="questions">
+		<h1 style = "margin-left: 25px"> Move book</h1>
+                <table>
+ 			<div class="autocomplete" style="width:300px;">
+			 <input id="search" type="text" name="search" placeholder="title">
+			 </div>
+			</form>
 
-    // Add a keyup event listener to our input element
-    var name_input = document.getElementById('input');
-    name_input.addEventListener("keyup", function(event){hinter(event)});
-
-    // create one global XHR object 
-    // so we can abort old requests when a new one is make
-    window.hinterXHR = new XMLHttpRequest();
-});
-
-// Autocomplete for form
-function hinter(event) {
-
-    // retireve the input element
-    var input = event.target;
-
-    // retrieve the datalist element
-    var huge_list = document.getElementById('huge_list');
-
-    // minimum number of characters before we start to generate suggestions
-    var min_characters = 0;
-
-    if (input.value.length < min_characters ) { 
-        return;
-    } else { 
-
-        // abort any pending requests
-        window.hinterXHR.abort();
-
-        window.hinterXHR.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-
-                // We're expecting a json response so we convert it to an object
-                var response = JSON.parse( this.responseText ); 
-
-                // clear any previously loaded options in the datalist
-                huge_list.innerHTML = "";
-
-                response.forEach(function(item) {
-                    // Create a new <option> element.
-                    var option = document.createElement('option');
-                    option.value = item;
-
-                    // attach the option to the datalist element
-                    huge_list.appendChild(option);
-                });
-            }
-        };
-
-        window.hinterXHR.open("GET", "/query.php?query=" + input.value, true);
-        window.hinterXHR.send()
-    }
-}
-</script>
+                </table>
+        	</div>
+	      <input type = "submit"  value = "Submit" name = "search" style = "margin:20px;margin-top:10px"/>
+      </form>
 </html>
 
 
