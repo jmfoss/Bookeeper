ff78558c6771324d2e30beeeacda7cd232270b95
Joshua Foss <jmfoss98@gmail.com>
2019-12-02 14:13:36 -0500

    Update dbm_searchbooks.php

--- dbm_searchbooks.php
+++ dbm_searchbooks.php
@@ -16,7 +16,6 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <head>
-  <title>Webslesson Tutorial | Autocomplete Textbox using Bootstrap Typehead with Ajax PHP</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
@@ -24,10 +23,9 @@
  <body>
   <br /><br />
   <div class="container" style="width:600px;">
-   <h2 align="center">Autocomplete Textbox using Bootstrap Typeahead with Ajax PHP</h2>
    <br /><br />
-   <label>Search Country</label>
-   <input type="text" name="country" id="country" class="form-control input-lg" autocomplete="off" placeholder="Type Country Name" />
+   <label>Search book title</label>
+   <input type="text" name="title" id="title" class="form-control input-lg" autocomplete="off" placeholder="Type your title" />
   </div>
  </body>
 </html>
@@ -35,7 +33,7 @@
 <script>
 $(document).ready(function(){
  
- $('#country').typeahead({
+ $('#title').typeahead({
   source: function(query, result)
   {
    $.ajax({
