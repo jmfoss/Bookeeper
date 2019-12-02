9a48e4a3e7ff4e7853ad3c92996b2d0a013dada3
Joshua Foss <jmfoss98@gmail.com>
2019-12-02 15:39:39 -0500

    Update dbm_searchbooks.php

--- dbm_searchbooks.php
+++ dbm_searchbooks.php
@@ -12,21 +12,27 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
- </head>
- <body>
-
-  <br /><br />
-  <div class="container" style="width:600px;">
-         <div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px"> </div>
+  <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" type="text/css" href="style.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px"> </div>
     <div class="topnav" id="myTopnav">
       <a href="dbm_main.php"> Home </a>
       <a href="dbm_searchbooks.php" class="active"> Search Books </a>
       <a href="dbm_addbook.php"> Add Books </a>
       <a href="dbm_addLibrary.php"> My Library </a>
-    </div>
+         </div>
+  </head>
+ <body>
+
+  <br /><br />
+  <div class="container" style="width:600px;">
+
+
    <br /><br />
-   <label>Search Country</label>
-   <input type="text" name="search" id="search" class="form-control input-lg" autocomplete="off" placeholder="Type book title" />
+   <label>Search a Book</label>
+   <input type="text" name="search" id="search" class="form-control input-lg" autocomplete="off" placeholder="Type Book Title" />
   </div>
  </body>
 </html>
