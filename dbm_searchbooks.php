905b5d4edfb8e47a83795982af3562bf0b10b2a4
Joshua Foss <jmfoss98@gmail.com>
2019-12-02 15:32:27 -0500

    Update dbm_searchbooks.php

--- dbm_searchbooks.php
+++ dbm_searchbooks.php
@@ -6,40 +6,28 @@
 
 
 <!DOCTYPE html>
-
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
  <head>
+  <title>Webslesson Tutorial | Autocomplete Textbox using Bootstrap Typehead with Ajax PHP</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
  </head>
  <body>
-   <body>
-    <div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px"> </div>
-    <div class="topnav" id="myTopnav">
-      <a href="dbm_main.php"> Home </a>
-      <a href="dbm_searchbooks.php" class="active"> Search Books </a>
-      <a href="dbm_addbook.php"> Add Books </a>
-      <a href="dbm_addLibrary.php"> My Library </a>
-    </div>
   <br /><br />
   <div class="container" style="width:600px;">
+   <h2 align="center">Autocomplete Textbox using Bootstrap Typeahead with Ajax PHP</h2>
    <br /><br />
-   <label>Search book title</label>
-   <input type="text" name="title" id="title" class="form-control input-lg" autocomplete="off" placeholder="Type your title" />
+   <label>Search Country</label>
+   <input type="text" name="country" id="country" class="form-control input-lg" autocomplete="off" placeholder="Type Country Name" />
   </div>
  </body>
-      </html>
+</html>
+
 <script>
 $(document).ready(function(){
  
- $('#title').typeahead({
+ $('#country').typeahead({
   source: function(query, result)
   {
    $.ajax({
@@ -59,4 +47,3 @@ $(document).ready(function(){
  
 });
 </script>
-
