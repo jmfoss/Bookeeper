a3dbf4069c9bf2914b3833d27e91af9c10995800
jmtucci <36246394+jmtucci@users.noreply.github.com>
2019-12-02 17:28:23 -0500

    Update dbm_main.php

--- dbm_main.php
+++ dbm_main.php
@@ -42,8 +42,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <!-- Table with links to ppt, tech doc and progress report -->
     <table align = "center">
       <tr>
-        <td style = "margin:20px; padding:20px; color:#2b3770; background-color:#b1bfcc"> <a href="isptechdoc.pdf"> Presentation </a> </td>
-        <td style = "margin:20px; padding:20px; color:#2b3770; background-color:#b1bfcc"> <a href="ppt.pdf"> Final Report </a> </td>
+        <td style = "margin:20px; padding:20px; color:#2b3770; background-color:#b1bfcc"> <a href="DBM_FinalPresentation.pdf"> Presentation </a> </td>
+        <td style = "margin:20px; padding:20px; color:#2b3770; background-color:#b1bfcc"> <a href="Stage6_jms590.docx"> Final Report </a> </td>
       </tr>
     </table>
        
