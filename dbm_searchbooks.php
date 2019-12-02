cd1c2e32b6137b94486139641198488100775c56
Joshua Foss <jmfoss98@gmail.com>
2019-12-02 15:41:13 -0500

    Update dbm_searchbooks.php

--- dbm_searchbooks.php
+++ dbm_searchbooks.php
@@ -16,7 +16,7 @@
  <meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" type="text/css" href="style.css">
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px"> </div>
+  <div id="title" align="center"> <img src="logo.png" style ="margin-top: 50px"> </div>
     <div class="topnav" id="myTopnav">
       <a href="dbm_main.php"> Home </a>
       <a href="dbm_searchbooks.php" class="active"> Search Books </a>
