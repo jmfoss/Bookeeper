150ad6fd12d3ffd8cb92efb9a22cd4a8196dc8a6
Joshua Foss <jmfoss98@gmail.com>
2019-12-03 00:11:19 -0500

    Update dbm_searchbooks.php

--- dbm_searchbooks.php
+++ dbm_searchbooks.php
@@ -26,7 +26,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
          
           $userQuery = trim($_POST["search"]);
           echo 'title:'.$userQuery.'*';
-          $query->setQuery('title:'.$userQuery);
+          $query->setQuery('title:*'.$userQuery.'*');
           $query->setStart(2)->setRows(20);
           $query->setFields(array('title', 'number_of_pages', 'isbn_10'));
           // this executes the query and returns the result
