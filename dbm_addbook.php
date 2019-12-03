a5288a0907e123bbdca655bbcc80698ed9dc896c
jmtucci <36246394+jmtucci@users.noreply.github.com>
2019-12-03 00:21:20 -0500

    Update dbm_addbook.php

--- dbm_addbook.php
+++ dbm_addbook.php
@@ -87,8 +87,8 @@
 <!DOCTYPE html>
 
 <html>
-  <title>Bookeeper</title>
   <head>
+    <title>Bookeeper</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
