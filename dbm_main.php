9e4baf27effca4dc1ee3d1adecaeb7eb3206a8ae
jmtucci <36246394+jmtucci@users.noreply.github.com>
2019-12-03 00:22:04 -0500

    Update dbm_main.php

--- dbm_main.php
+++ dbm_main.php
@@ -17,6 +17,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
 <html>
   <head>
+    <title>Bookeeper</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
